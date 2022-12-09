020a942c601b8f61b6eeca911f77c111086f5fe4
conuko <constantinu93@gmail.com>
2022-12-09 12:27:02 +0100

    feat(guestbook): add protected mutation

---
+++ src/server/trpc/router/guestbook.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { router, protectedProcedure } from "../trpc";
+
+export const guestbookRouter = router({
+  postMessage: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.guestbook.create({
+          data: {
+            name: input.name,
+            message: input.message,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
