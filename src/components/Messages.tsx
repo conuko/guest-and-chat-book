550792d73797a1d21b4182162c33cacc92422deb
Constantin Unterkofler <constantin.unterkofler@jvm.de>
2023-01-12 12:40:39 +0100

    feat(Messages): add edit button

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,5 +1,6 @@
 import { trpc } from "../utils/trpc";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Messages = () => {
   const { data: messages, isLoading } = trpc.guestbook.getAll.useQuery();
@@ -24,7 +25,33 @@ const Messages = () => {
   return (
     <div className="flex flex-col gap-4">
       {messages?.map((msg, index) => {
-        return (
+        /* 
+        if the message belongs to the user,
+        make it possible to click and edit message and show delete button:
+        */
+        return msg.userId === session?.user?.id ? (
+          <Link
+            className="flex items-center justify-between gap-2 rounded-md border-2 border-zinc-800 p-6 hover:border-zinc-500"
+            key={index}
+            href={`/messages/${msg.id}`}
+          >
+            <div>
+              <p>{msg.message}</p>
+              <span className="text-gray-400">- {msg.name}</span>
+            </div>
+            {/*            <div>
+              <button
+                className="pl-5 text-red-400 hover:text-red-500"
+                onClick={() => handleDelete(msg.id)}
+              >
+                X
+              </button>
+            </div> */}
+          </Link>
+        ) : (
+          /* 
+           if the message does not belong to the user, just display the message:
+          */
           <div
             className="flex items-center justify-between gap-2 rounded-md border-2 border-zinc-800 p-6"
             key={index}
@@ -33,16 +60,6 @@ const Messages = () => {
               <p>{msg.message}</p>
               <span className="text-gray-400">- {msg.name}</span>
             </div>
-            {msg.userId === session?.user?.id && (
-              <div>
-                <button
-                  className="pl-5 text-red-400 hover:text-red-500"
-                  onClick={() => handleDelete(msg.id)}
-                >
-                  X
-                </button>
-              </div>
-            )}
           </div>
         );
       })}
