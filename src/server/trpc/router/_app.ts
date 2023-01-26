f8c1e5acc962b1c1feb59e937e6322e60e8440d2
Constantin Unterkofler <constantin.unterkofler@jvm.de>
2023-01-26 11:57:41 +0100

    feat(stripe): add user router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -2,9 +2,11 @@ import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { guestbookRouter } from "./guestbook";
 import { stripeRouter } from "./stripe";
+import { userRouter } from "./user";
 
 export const appRouter = router({
   stripe: stripeRouter,
+  user: userRouter,
   guestbook: guestbookRouter,
   auth: authRouter,
 });
